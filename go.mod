7c0326c8fee935c3f0564df879133dd5e9a0acf6
Larry Nguyen <larrynguyen33@gmail.com>
2020-05-03 04:13:47 -0700

    list videos

---
+++ go.mod
@@ -0,0 +1,5 @@
+module martini.com/project/videoservice
+
+go 1.14
+
+require github.com/gin-gonic/gin v1.6.2
---
+++ server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"martini.com/project/videoservice/controller"
+	"martini.com/project/videoservice/service"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
+	})
+
+	server.Run(":8282")
+}
